18c620285703578e5f9621c2f73c266f1f946c08
ocroopesh <roopesh.madhesia@ordercup.com>
2018-04-17 21:49:20 +0530

    connection change

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'icargov1');
-define('DB_PASSWORD', 'pcs@pcs');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'roopesh@pcs');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargo_live_v1');
+define('DB_NAME', 'iCargo_v9');
 define('DB_PREFIX', 'icargo_');
 
 ?>
