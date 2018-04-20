799b58c5afdbc6aeb33d75af8c4a36029e68c1fe
kavita <kavita.pandey@ordercup.com>
2018-04-20 18:52:14 +0530

    config updated

--- config.php
+++ config.php
@@ -9,10 +9,10 @@ define('DB_NAME', 'iCago_v1');
 define('DB_PREFIX', 'icargo_');*/
 
 
-define('DB_USERNAME', 'icargo_dev');
-define('DB_PASSWORD', 'icargodev');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargo_dev');
+define('DB_NAME', 'icargo');
 define('DB_PREFIX', 'icargo_');
 
 ?>
