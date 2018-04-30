313c7a99eb6fef0d538b15e986739df6475a016e
nishant <nishant.verma@ordercup.com>
2018-04-30 13:17:05 +0530

    workspace fixed

--- config.php
+++ config.php
@@ -7,12 +7,10 @@ define('DB_PASSWORD', 'Icar@#$!');
 define('DB_HOST', '54.191.172.136');
 define('DB_NAME', 'iCago_v1');
 define('DB_PREFIX', 'icargo_');*/
-
-
-define('DB_USERNAME', 'icargo_v2');
-define('DB_PASSWORD', 'baner@123');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargo');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'baner123');
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'icargo_v3');
 define('DB_PREFIX', 'icargo_');
 
 ?>
