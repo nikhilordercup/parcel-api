a88a6cd57cd79b96714cb4f16400e16c115813bb
ockavita <kavita.pandey@ordercup.com>
2018-06-01 14:33:40 +0530

    latest code merge

--- config.php
+++ config.php
@@ -8,8 +8,8 @@ define('DB_HOST', '54.191.172.136');
 define('DB_NAME', 'iCago_v1');
 define('DB_PREFIX', 'icargo_');*/
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'roopesh@pcs');
-define('DB_HOST', '127.0.0.1:3306');
-define('DB_NAME', 'icargo_dev');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'icargo');
 define('DB_PREFIX', 'icargo_');
 ?>
