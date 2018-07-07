f8dc1d3c5113944e4b434f89d4f084efdfd88694
msnainps <mandeep@perceptive-solutions.com>
2018-07-07 12:41:55 +0530

    Driver common sql generated

--- v1/module/driver/DriverController.php
+++ v1/module/driver/DriverController.php
@@ -15,42 +15,48 @@ class DriverController extends Icargo {
 
     private $_user_id;
     protected $_parentObj;
-    
     private $_db;
     private $_app;
     private $_requestParams;
-    private $_table='user_grid_states';
+    private $_table = 'user_grid_states';
+
     /**
      * Driver Controller constructor.
      */
     private function __construct($app) {
         $this->_db = new DbHandler();
         $this->_app = $app;
-        $this->_requestParams=json_decode($this->_app->request->getBody());
+        $this->_requestParams = json_decode($this->_app->request->getBody());
     }
+
     /**
      * Fetch All Driver List
      */
-    public function getAllDrivers(){
-        
+    public function getAllDrivers($companyId) {
+        $sql = "SELECT U.* FROM `" . DB_PREFIX . "users` AS U JOIN " . DB_PREFIX . "company_users  AS CU ON U.id=CU.user_id 
+WHERE  user_level=4 AND CU.company_id=$companyId";
     }
+
     /**
      * Fetch All Active Drivers
      */
-    public function getActiveDrivers(){
-        
+    public function getActiveDrivers($companyId) {
+        $sql = "SELECT U.* FROM `" . DB_PREFIX . "users` AS U JOIN " . DB_PREFIX . "company_users  AS CU ON U.id=CU.user_id 
+WHERE device_token_id IS NOT NULL AND user_level=4 AND CU.company_id=$companyId";
     }
+
     /**
      * Fetch all assigned routes for active drivers.
      */
-    public function getActiveDriverRoutes(){
+    public function getActiveDriverRoutes() {
         
     }
+
     /**
      * Fetch all routes information according to date and grouped with assigned driver.
      */
-    public function getDayRoutes(){
-        
+    public function getDayRoutes($companyId,$date) {
+        $sql="SELECT * FROM `icargo_shipment_route` WHERE company_id=$companyId AND is_active='Y' AND service_date='$date'";
     }
 
 }
