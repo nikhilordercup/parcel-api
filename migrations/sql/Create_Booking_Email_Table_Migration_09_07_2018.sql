df535cd1b23c1368b52d91c6e31d55c391bc1d6d
nishant <nishant.verma@ordercup.com>
2018-07-10 13:12:07 +0530

    migration sql updated

--- migrations/sql/Create_Booking_Email_Table_Migration_09_07_2018.sql
+++ migrations/sql/Create_Booking_Email_Table_Migration_09_07_2018.sql
@@ -1,8 +1,14 @@
-CREATE TABLE IF NOT EXISTS `icargo_custom_filter_config` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `filter_name` varchar(250) NOT NULL,
-  `filter_slug` varchar(250) NOT NULL,
-  `filter_config` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+ALTER TABLE `icargo_quote_service`
+ADD COLUMN `email` VARCHAR(100) NULL DEFAULT NULL COMMENT 'Quotation email id' AFTER `expiry_date`;
+
+ALTER TABLE `icargo_quote_service`
+ADD COLUMN `booking_type` VARCHAR(20) NOT NULL DEFAULT 'sameday' AFTER `email`;
+
+ALTER TABLE `icargo_quote_shipment`
+ADD COLUMN `json_string` TEXT NULL DEFAULT NULL AFTER `warehouse_id`;
+
+ALTER TABLE `icargo_quote_service`
+ADD COLUMN `warehouse_id` INT NULL DEFAULT 0 AFTER `email`,
+ADD COLUMN `company_id` INT NULL DEFAULT 0 AFTER `warehouse_id`;
+
+ALTER TABLE `icargo_quote_service` CHANGE `service_opted` `service_opted` LONGTEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
