f65611ea900d883303c43a5a9d8a53affaf4ae32
msnainps <mandeep@perceptive-solutions.com>
2018-07-17 16:23:23 +0530

    Time Intervel shipment Fetch

--- v1/module/driver/DriverController.php
+++ v1/module/driver/DriverController.php
@@ -73,15 +73,16 @@ WHERE device_token_id IS NOT NULL AND user_level=4 AND CU.company_id=$companyId"
         $sql="SELECT * FROM `".DB_PREFIX."shipment_route` WHERE company_id=$companyId  AND service_date='$date'";
 //        $sql="SELECT * FROM `".DB_PREFIX."shipment_route` WHERE company_id=$companyId  AND driver_id > 0";
         $shipmentSql="SELECT * FROM `".DB_PREFIX."shipment` WHERE shipment_routed_id IN ("
-                . "SELECT shipment_id FROM `".DB_PREFIX."shipment_route` WHERE "
+                . "SELECT shipment_route_id FROM `".DB_PREFIX."shipment_route` WHERE "
                 . "company_id=$companyId  AND service_date='$date')";
         $rec =$this->_db->getAllRecords($sql);
         $shipments=$this->_db->getAllRecords($shipmentSql);
-        print_r($rec);print_r($shipments);exit;
         $result=array();
-        foreach($rec as $r){
+        foreach($rec as $k=>$r){
             foreach ($shipments as $s){
-                
+                if($r['shipment_route_id']==$s['shipment_routed_id']){
+                    $rec[$k]['shipments'][]=$s;
+                }                
             }
         }
         return $rec;
