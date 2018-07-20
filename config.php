8bb169667b4a632c87a38fa342e800b788c9cc9c
nishant <nishant.verma@ordercup.com>
2018-07-20 18:53:40 +0530

    missing config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Database configuration
+ */
+define('DB_USERNAME', 'icargo_dev');
+define('DB_PASSWORD', 'icargodev');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'icargo_dev');
+define('DB_PREFIX', 'icargo_');
+?>
