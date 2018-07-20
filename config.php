4afe251b2b818e3cb7c5a8fd3ba31271b4c25341
ocnishant <nishant.verma@ordercup.com>
2018-07-20 20:43:04 +0530

    Update config.php
    db connection fixed

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /*
  * Database configuration
  */
-define('DB_USERNAME', 'icargo_dev');
-define('DB_PASSWORD', 'icargodev');
+define('DB_USERNAME', 'app_stable');
+define('DB_PASSWORD', 'pcs@pcs');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargo_dev');
+define('DB_NAME', 'app_stable');
 define('DB_PREFIX', 'icargo_');
 ?>
