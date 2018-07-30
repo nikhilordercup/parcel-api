2d9913319f7626b9acf9d80f7f368da42cb65b99
ocnishant <nishant.verma@ordercup.com>
2018-07-30 13:40:35 +0530

    Update Update_System_Configuration_Table_30_07_2018.sql
    mysql compatible query changed

--- migrations/sql/Update_System_Configuration_Table_30_07_2018.sql
+++ migrations/sql/Update_System_Configuration_Table_30_07_2018.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `icargo_system_configuration` CHANGE COLUMN `extra_data` `extra_data` TEXT NULL DEFAULT NULL ;
+ALTER TABLE `icargo_system_configuration` CHANGE `extra_data` `extra_data` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
