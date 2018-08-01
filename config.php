a825324c2da8e115a2ad857b507bddb65191726e
nikhilordercup <nikhil@perceptive-solutions.com>
2018-08-01 17:18:27 +0530

    rate list test

--- config.php
+++ config.php
@@ -2,15 +2,15 @@
 /*
  * Database configuration
  */
-/*define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '123456');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'icargo_k');
-define('DB_PREFIX', 'icargo_');*/
+define('DB_PREFIX', 'icargo_');
 
-define('DB_USERNAME','icargoN'); 
+/*define('DB_USERNAME','icargoN'); 
 define('DB_PASSWORD', 'icargoN');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'icargoN');
-define('DB_PREFIX', 'icargo_');
+define('DB_PREFIX', 'icargo_');*/
 
