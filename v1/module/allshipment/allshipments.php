c2a6a898b9b14228d3d5ec479491b97f584efac3
nikhilordercup <nikhil@perceptive-solutions.com>
2018-08-21 16:06:36 +0530

    conflict resolve

--- v1/module/allshipment/allshipments.php
+++ v1/module/allshipment/allshipments.php
@@ -199,21 +199,15 @@ class allShipments extends Icargo
                     $data['action'] = 'nextday';
                     if (array_key_exists('P', $innerval['NEXT'])) {
                         foreach ($innerval['NEXT']['P'] as $pickupkey => $pickupData) {
-							$labelArr = json_decode($pickupData['label_json']);
-<<<<<<< HEAD
+                            $labelArr = json_decode($pickupData['label_json']);
 
-							if(is_object($labelArr) && count($labelArr)>0){
-								$collectionReference = isset($labelArr->label->collectionjobnumber) ? $labelArr->label->collectionjobnumber : $labelArr->label->tracking_number;
-							}else{
-								$collectionReference = "";
-							}
-=======
-							if(is_object($labelArr) && count($labelArr)>0)
-								$collectionReference = isset($labelArr->label->collectionjobnumber) ? $labelArr->label->collectionjobnumber : $labelArr->label->tracking_number;
-							else
-								$collectionReference = "";
->>>>>>> 7d4951c434d9e6de364ef4caae85b07d2aab96c7
-							//}
+
+                            if(is_object($labelArr) && count($labelArr)>0){
+                                    $collectionReference = isset($labelArr->label->collectionjobnumber) ? $labelArr->label->collectionjobnumber : $labelArr->label->tracking_number;
+                            }else{
+                                    $collectionReference = "";
+                            }
+
                             $data['customer']    = $pickupData['shipment_customer_name'];
                             $data['account']     = $pickupData['shipment_customer_account'];
                             $data['service']     = $pickupData['shipment_service_name'];
