f976fb6137aabac32ba349c45194278b83b1e210
ocnishant <nishant.verma@ordercup.com>
2018-08-21 21:50:56 +0530

    Update icargo_shipment_service_add_tracking_code.sql
    When booking done then the initial tracking status should be INFO_RECEIVED

--- migrations/sql/icargo_shipment_service_add_tracking_code.sql
+++ migrations/sql/icargo_shipment_service_add_tracking_code.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `icargo_shipment_service` ADD `tracking_code` VARCHAR(45) NULL DEFAULT NULL AFTER `status`;
+ALTER TABLE `icargo_shipment_service` CHANGE `tracking_code` `tracking_code` VARCHAR(45) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT 'INFO_RECEIVED';
