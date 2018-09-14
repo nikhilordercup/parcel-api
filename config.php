e8832334180aadfc5b82aaa21278942c9bdb4393
ockavita <kavita.pandey@ordercup.com>
2018-09-14 17:37:09 +0000

    connection change

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /* 
  * Database configuration
@@ -10,16 +9,3 @@ define('DB_NAME', 'icargoN');
 define('DB_PREFIX', 'icargo_'); 
 define('LABEL_URL', '/dev/');  
 ?>
-=======
-<?php
-/* 
- * Database configuration
- */
-define('DB_USERNAME', 'icargoN');
-define('DB_PASSWORD', 'icargoN');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargoN');
-define('DB_PREFIX', 'icargo_'); 
-define('LABEL_URL', '/dev/'); 
-?>  
->>>>>>> 5bb2d44efb20276cb287f32b47a82b295dbdce49
