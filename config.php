79c3bce554d64d43b936f2387f4563e7cd5aa136
ockavita <kavita.pandey@ordercup.com>
2018-09-18 09:05:13 +0000

    direct server editing

--- config.php
+++ config.php
@@ -9,10 +9,10 @@ define('DB_NAME', 'icargo_n');
 define('DB_PREFIX', 'icargo_');
 define('LABEL_URL', '');*/
 
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'icargoN');
+define('DB_PASSWORD', 'icargoN');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargon');
+define('DB_NAME', 'icargoN');
 define('DB_PREFIX', 'icargo_');
 define('LABEL_URL', '/dev/');
 ?>
