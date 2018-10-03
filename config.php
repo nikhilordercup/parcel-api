656e4a6285c45163d449b2b0eb07e3ccc290f1c1
nikhilordercup <nikhil@perceptive-solutions.com>
2018-10-03 12:19:08 +0530

    db configure

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ define('DB_PASSWORD', 'icargoN');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'icargoN');
 define('DB_PREFIX', 'icargo_'); 
-define('LABEL_URL', '/dev_git/');  
+define('LABEL_URL', '/dev/');  
 ?>
 
 
