5f8a22251c6d0cade9a1119b8ca077429dadb417
kavitapcs <kavita.p@perceptive-solutions.com>
2018-10-12 11:44:29 +0530

    firebase code setup

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 /*
  * Database configuration
  */
-define('DB_USERNAME', 'icargoN');
-define('DB_PASSWORD', 'icargoN');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargoN');
+define('DB_NAME', 'icargo_live');
 define('DB_PREFIX', 'icargo_'); 
 define('LABEL_URL', '/dev/');  
 ?>
