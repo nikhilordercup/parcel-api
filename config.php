96d306b646764b7b6f3822047fdfed67ae41f923
nishant <nishant.verma@ordercup.com>
2018-10-15 11:54:12 +0530

    app-allignment db connection changed

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 /*
  * Database configuration
  */
-define('DB_USERNAME', 'icargoN');
-define('DB_PASSWORD', 'icargoN');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargoN');
-define('DB_PREFIX', 'icargo_'); 
-define('LABEL_URL', '/dev/');  
+ define('DB_USERNAME', 'icargoN');
+ define('DB_PASSWORD', 'icargoN');
+ define('DB_HOST', 'localhost');
+ define('DB_NAME', 'icargoN');
+ define('DB_PREFIX', 'icargo_');
+ define('LABEL_URL', '/app-alignment/');
 ?>
 
 
