3709d69478476b8091d09ecc8191bbe33cc61a05
msnainps <mandeep@perceptive-solutions.com>
2018-11-20 15:17:17 +0530

    Exit removed

--- v1/module/coreprime/api.php
+++ v1/module/coreprime/api.php
@@ -485,7 +485,6 @@ class Module_Coreprime_Api extends Icargo
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec($ch);
         curl_close($ch);
-//        exit($url);
         return $server_output;
     }
     public function mergePrice(&$finalArray,$price){
