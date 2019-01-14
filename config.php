d60980a85c1a63f563641a566ef8af45da0212b0
nikhilordercup <nikhil.kumar@ordercup.com>
2019-01-14 13:05:00 +0000

    permission updated on server

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 /* 
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'app_stable');
+define('DB_PASSWORD', 'pcs@pcs');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargo_tuff');
+define('DB_NAME', 'app_stable');
 define('DB_PREFIX', 'icargo_');
 define('PDFURL','http://'.$_SERVER['HTTP_HOST'].'/label');
 global $_GLOBAL_CONTAINER;
