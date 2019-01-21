a191ea0a81e1f7cacf0e31a2ad127491d347c614
ocroopesh <roopesh.madhesia@ordercup.com>
2019-01-21 21:20:19 +0530

    connection change

--- config.php
+++ config.php
@@ -2,9 +2,13 @@
 /*
  * Database configuration
  */
- define('DB_USERNAME', 'app_stable');
- define('DB_PASSWORD', 'pcs@pcs');
- define('DB_HOST', 'localhost');
- define('DB_NAME', 'app_stable');
- define('DB_PREFIX', 'icargo_');
-?>
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'roopesh@pcs');
+define('DB_HOST', '192.168.64.2');
+define('DB_NAME', 'icargo_live_v3_1');
+define('DB_PREFIX', 'icargo_');
+define('PDFURL','https://'.$_SERVER['HTTP_HOST'].'/live/label');
+global $_GLOBAL_CONTAINER;
+?>  
+    
+
