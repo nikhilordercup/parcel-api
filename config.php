78907a1804ea19c34b7c47f75001a0a410d279bf
msnainps <mandeep@perceptive-solutions.com>
2019-01-25 10:52:20 +0530

    Live Env Config

--- config.php
+++ config.php
@@ -2,10 +2,12 @@
 /*
  * Database configuration
  */
- define('DB_USERNAME', 'app_stable');
- define('DB_PASSWORD', 'pcs@pcs');
- define('DB_HOST', 'localhost');
- define('DB_NAME', 'app_stable');
- define('DB_PREFIX', 'icargo_');
- define('PDFURL','https://'.$_SERVER['HTTP_HOST'].'/app-allignment/parcel-api/label');
- global $_GLOBAL_CONTAINER;
+define('DB_USERNAME', 'icargo_live_v3_1');
+define('DB_PASSWORD', 'icargo_live_345');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'icargo_live_v3_1');
+define('DB_PREFIX', 'icargo_');
+define('PDFURL', 'https://' . $_SERVER['HTTP_HOST'] . '/live/label');
+define('BASE_PATH', __DIR__);
+define('LABEL_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'label');
+global $_GLOBAL_CONTAINER;
