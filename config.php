0fe8f26976e1afc3599e2dc038283ec9b9842af7
msnainps <mandeep@perceptive-solutions.com>
2019-02-06 18:31:35 +0530

    composer fix

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 /*
  * Database configuration
  */
-define('DB_USERNAME', 'icargo_live_v3_1');
-define('DB_PASSWORD', 'icargo_live_345');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargo_live_v3_1');
+define('DB_NAME', 'icargo_tuff');
 define('DB_PREFIX', 'icargo_');
-define('PDFURL', 'https://' . $_SERVER['HTTP_HOST'] . '/live/label');
+define('PDFURL', 'https://' . $_SERVER['HTTP_HOST'] . '/label');
 define('BASE_PATH', __DIR__);
 define('LABEL_PATH', BASE_PATH . DIRECTORY_SEPARATOR . 'label');
 global $_GLOBAL_CONTAINER;
