df50c8c20aa62cc9e03e583aa023e53a447481ca
msnainps <mandeep@perceptive-solutions.com>
2019-02-12 15:39:13 +0530

    Env Changed

--- config.php
+++ config.php
@@ -2,14 +2,14 @@
 /* 
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'app_stable');
+define('DB_PASSWORD', 'pcs@pcs');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'icargo_tuff');
+define('DB_NAME', 'app_stable');
 define('DB_PREFIX', 'icargo_');
 define('PDFURL','http://'.$_SERVER['HTTP_HOST'].'/label');
 define('BASE_PATH',__DIR__);
 define('LABEL_PATH',BASE_PATH.DIRECTORY_SEPARATOR.'label');
-define('BASE_URL','http://api.icargo.in/');
-define('UI_BASE_URL','http://localhost/icargo/');
+define('BASE_URL','https://api.instadispatch.com/app-allignment/parcel-api/');
+define('UI_BASE_URL','https://app-tree.co.uk/app-stable/');
 global $_GLOBAL_CONTAINER;
